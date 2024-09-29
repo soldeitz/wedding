3f23924ed10ffd53b01e69194c3eca6280ad544d
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-09-29 18:34:06 +0200

    title

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,7 +13,8 @@ export default function Index() {
 			<div className="flex flex-col items-center gap-16">
 				<header className="flex flex-col items-center gap-9">
 					<h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-						Welcome to <span className="sr-only">Remix</span>
+						Welcome to{" "}
+						<span className="sr-only">Irene e Carlo</span>
 					</h1>
 					<div className="h-[144px] w-[434px]">
 						<img
@@ -28,11 +29,6 @@ export default function Index() {
 						/>
 					</div>
 				</header>
-				<nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-					<p className="leading-6 text-gray-700 dark:text-gray-200">
-						Irene e Carlo
-					</p>
-				</nav>
 			</div>
 		</div>
 	);
