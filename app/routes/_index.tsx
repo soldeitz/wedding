1de1de027c387b1acf81ba8bc898688d3f377daf
micheleAndreata <michele.andreata@studenti.unimi.it>
2024-10-07 00:14:04 +0200

    this commit should not exist

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,35 +1,26 @@
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
-	return [
-		{ title: "Irene e Carlo" },
-		{ name: "description", content: "Sito Matrimonio di Irene e Carlo" },
-	];
+  return [
+    { title: "Irene e Carlo" },
+    { name: "description", content: "Sito Matrimonio di Irene e Carlo" },
+  ];
 };
 
 export default function Index() {
-	return (
-		<div className="flex h-screen items-center justify-center">
-			<div className="flex flex-col items-center gap-16">
-				<header className="flex flex-col items-center gap-9">
-					<h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-						Welcome to{" "}
-						<span className="sr-only">Irene e Carlo</span>
-					</h1>
-					<div className="h-[144px] w-[434px]">
-						<img
-							src="/logo-light.png"
-							alt="Remix"
-							className="block w-full dark:hidden"
-						/>
-						<img
-							src="/logo-dark.png"
-							alt="Remix"
-							className="hidden w-full dark:block"
-						/>
-					</div>
-				</header>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <div className="flex flex-col items-center gap-16">
+        <h1 className="leading text-5xl md:text-8xl font-bold text-gray-800 dark:text-gray-100">
+          Irene e Carlo
+        </h1>
+        <div className="flex space-x-2 justify-center items-center bg-white dark:invert">
+          <span className="sr-only">Loading...</span>
+          <div className="h-6 w-6 md:h-8 md:w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+          <div className="h-6 w-6 md:h-8 md:w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+          <div className="h-6 w-6 md:h-8 md:w-8 bg-black rounded-full animate-bounce"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
