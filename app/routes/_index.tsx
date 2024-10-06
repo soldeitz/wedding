69ea119c8edc94411848677bdc120a36399b3c4d
lukeku62 (Luca Reverberi) <luca.reverberi62@gmail.com>
2024-10-07 01:18:34 +0200

    feat: update wip

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import loading from "../assets/images/loading.gif";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,15 +12,16 @@ export default function Index() {
   return (
     <div className="flex h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-16">
-        <h1 className="leading text-5xl md:text-8xl font-bold text-gray-800 dark:text-gray-100">
+        <h1 className="leading text-5xl md:text-8xl font-bold text-gray-800 text-slate-900">
           Irene e Carlo
         </h1>
-        <div className="flex space-x-2 justify-center items-center bg-white dark:invert">
+        <img src={loading} alt="loading" />
+        {/* <div className="flex space-x-2 justify-center items-center bg-white dark:invert">
           <span className="sr-only">Loading...</span>
           <div className="h-6 w-6 md:h-8 md:w-8 bg-black rounded-full animate-bounce [animation-delay:-0.3s]"></div>
           <div className="h-6 w-6 md:h-8 md:w-8 bg-black rounded-full animate-bounce [animation-delay:-0.15s]"></div>
           <div className="h-6 w-6 md:h-8 md:w-8 bg-black rounded-full animate-bounce"></div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
