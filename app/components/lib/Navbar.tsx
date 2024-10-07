aad56516285c4ef373818b48c63034013ca23807
lukeku62 (Luca Reverberi) <luca.reverberi62@gmail.com>
2024-10-07 02:04:38 +0200

    feat: animation logo + quote

--- app/components/lib/Navbar.tsx
+++ app/components/lib/Navbar.tsx
@@ -1,12 +1,14 @@
+import { Link } from "@remix-run/react";
+
 export const Navbar = () => {
   return (
     <nav className="absolute top-0 left-0 right-0 w-100 flex justify-between mx-10 mt-7 text-base-200">
-      <a href="/">Home</a>
-      <a href="cerimonia">Cerimonia</a>
-      <a href="rsvp">RSVP</a>
-      <a href="lista-nozze">Lista Nozze</a>
-      <a href="ricevimento">Ricevimento</a>
-      <a href="galleria">Galleria</a>
+      <Link to="/">Home</Link>
+      <Link to="cerimonia">Cerimonia</Link>
+      <Link to="rsvp">RSVP</Link>
+      <Link to="lista-nozze">Lista Nozze</Link>
+      <Link to="ricevimento">Ricevimento</Link>
+      <Link to="galleria">Galleria</Link>
     </nav>
   );
 };
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import logo from "../assets/images/logo.png";
 export const meta: MetaFunction = () => {
   return [
     { title: "Irene e Carlo" },
@@ -9,26 +9,35 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
-      <div
-        className="hero h-screen"
-        style={{
-          backgroundImage: "url(hero-index.jpeg)",
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Irene e Carlo</h1>
-            <p className="text-lg font-bold">26/12/2024</p>
-            <p className="mt-10">
-              Ardi, non troppo vicino al cielo e non troppo lontano. Ricordati,
-              cuore, di quello sguardo in cui ti attende tutta l’eternità.
-              <span className="text-right">Karol Wojtyła</span>
-            </p>
-          </div>
+    <div className="h-screen">
+      <div className="flex flex-col justify-evenly h-full">
+        <img src={logo} alt="logo" className="animate-[slideUp_1s_ease-out]" />
+        <div className="fade-in mx-auto p-4">
+          <p className="text-center text-2xl">
+            Ardi, non troppo vicino al cielo e non troppo lontano. Ricordati,
+            cuore, di quello sguardo in cui ti attende tutta l’eternità.
+          </p>
+          <p className="w-full text-end">Karol Wojtyła</p>
         </div>
       </div>
-    </>
+    </div>
+    // <div
+    //   className="hero h-screen"
+    //   style={{
+    //     backgroundImage: "url(hero-index.jpeg)",
+    //   }}>
+    //   <div className="hero-overlay bg-opacity-60"></div>
+    //   <div className="hero-content text-neutral-content text-center">
+    //     <div className="max-w-md">
+    //       <h1 className="mb-5 text-5xl font-bold">Irene e Carlo</h1>
+    //       <p className="text-lg font-bold">26/12/2024</p>
+    //       <p className="mt-10">
+    //         Ardi, non troppo vicino al cielo e non troppo lontano. Ricordati,
+    //         cuore, di quello sguardo in cui ti attende tutta l’eternità.
+    //         <span className="text-right">Karol Wojtyła</span>
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
