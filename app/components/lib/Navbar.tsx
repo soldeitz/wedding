99d45942ab5e9231c381e155569c24c9ce2ca302
lukeku62 (Luca Reverberi) <luca.reverberi62@gmail.com>
2024-10-07 02:37:10 +0200

    feat: homepage mobile + desktop

--- app/components/lib/Navbar.tsx
+++ app/components/lib/Navbar.tsx
@@ -1,14 +1,81 @@
 import { Link } from "@remix-run/react";
+import { MdOutlineMenu } from "react-icons/md";
 
 export const Navbar = () => {
   return (
-    <nav className="absolute top-0 left-0 right-0 w-100 flex justify-between mx-10 mt-7 text-base-200">
-      <Link to="/">Home</Link>
-      <Link to="cerimonia">Cerimonia</Link>
-      <Link to="rsvp">RSVP</Link>
-      <Link to="lista-nozze">Lista Nozze</Link>
-      <Link to="ricevimento">Ricevimento</Link>
-      <Link to="galleria">Galleria</Link>
-    </nav>
+    <>
+      {/* // MOBILE */}
+      <div className="drawer drawer-end justify-end md:hidden">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <label htmlFor="my-drawer-4" className="drawer-button btn-ghost btn">
+            <MdOutlineMenu className="text-2xl" />
+          </label>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-4"
+            aria-label="close sidebar"
+            className="drawer-overlay"></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <Link
+                to="/"
+                onClick={() => document.getElementById("my-drawer-4")?.click()}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="cerimonia"
+                onClick={() => document.getElementById("my-drawer-4")?.click()}>
+                Cerimonia
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="rsvp"
+                onClick={() => document.getElementById("my-drawer-4")?.click()}>
+                RSVP
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="lista-nozze"
+                onClick={() => document.getElementById("my-drawer-4")?.click()}>
+                Lista Nozze
+              </Link>
+            </li>
+            <li>
+              <Link
+                
+                to="ricevimento"
+                onClick={() => document.getElementById("my-drawer-4")?.click()}>
+                Ricevimento
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="galleria"
+                onClick={() => document.getElementById("my-drawer-4")?.click()}>
+                Galleria
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* // DESKTOP */}
+
+      <nav className="absolute top-0 left-0 right-0 w-100 justify-between mx-10 mt-7 text-slate-900 hidden md:flex">
+        <Link to="/">Home</Link>
+        <Link to="cerimonia">Cerimonia</Link>
+        <Link to="rsvp">RSVP</Link>
+        <Link to="lista-nozze">Lista Nozze</Link>
+        <Link to="ricevimento">Ricevimento</Link>
+        <Link to="galleria">Galleria</Link>
+      </nav>
+    </>
   );
 };
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import logo from "../assets/images/logo.png";
+import home1 from "../assets/images/home1_mobile.png";
+import home1_desktop from "../assets/images/home1_desktop.png";
 export const meta: MetaFunction = () => {
   return [
     { title: "Irene e Carlo" },
@@ -9,16 +11,30 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="h-screen">
-      <div className="flex flex-col justify-evenly h-full">
-        <img src={logo} alt="logo" className="animate-[slideUp_1s_ease-out]" />
-        <div className="fade-in mx-auto p-4">
-          <p className="text-center text-2xl">
-            Ardi, non troppo vicino al cielo e non troppo lontano. Ricordati,
-            cuore, di quello sguardo in cui ti attende tutta l’eternità.
-          </p>
-          <p className="w-full text-end">Karol Wojtyła</p>
+    <div className="flex flex-col">
+      <div className="h-screen md:mx-auto ">
+        <div className="flex flex-col justify-evenly h-full items-center">
+          <img
+            src={logo}
+            alt="logo"
+            className="animate-[slideUp_1s_ease-out] md:w-1/3"
+          />
+          <div className="fade-in mx-auto p-4 md:w-1/3">
+            <p className="text-center text-2xl md:text-4xl">
+              Ardi, non troppo vicino al cielo e non troppo lontano. Ricordati,
+              cuore, di quello sguardo in cui ti attende tutta l’eternità.
+            </p>
+            <p className="w-full text-end">Karol Wojtyła</p>
+          </div>
         </div>
+      </div>
+      <div className="hidden md:block">
+        <div className="flex flex-col justify-evenly h-full">
+          <img src={home1_desktop} alt="home1" />
+        </div>
+      </div>
+      <div className="block md:hidden">
+        <img src={home1} alt="home1" />
       </div>
     </div>
     // <div
